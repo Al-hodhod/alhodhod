11477c6a2153ec3f936b220d849bc75f8903baf9
Faraz Ahmed <faraz.ahmed@arkhitech.com>
2023-10-23 03:22:05 +0500

    First rendition complete

--- src/app/page.styled.tsx
+++ src/app/page.styled.tsx
@@ -22,13 +22,14 @@ export const ChooseTheFirstLetter = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  padding: 0 4.58vw;
 `;
 
 export const LetterSelection = styled.div`
   width: 78.125vw;
   background: #FFFFFF;
   border-radius: 21.3906px;
-  padding: 12.5px 88px 12.5px 88px;
+  padding: 0.65vh 0;
 `
 export const LetterRow = styled.div`
   display: flex;
@@ -86,6 +87,101 @@ export const AdvertisementImageTag = styled(Image)`
   border-radius: 20px;
 `;
 
-export const ListOfSymbolsForLetter = styled.div`
-  
+export const ListOfSymbolsForLetterSection = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 8.54vh;
+`;
+
+export const LettersList = styled.div`
+  width: 78.125vw;
+  background-color: white;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-bottom: 32px;
+`;
+
+export const SelectedLetterSection = styled.div`
+  background-color: #F9F9F9;
+  width: 174.81px;
+  height: 230.1px;
+  top: 2183.12px
+  left: 873.04px
+  border-radius: 0px, 0px, 80.27px, 80.27px
+  font-size: 167px;
+  font-weight: 700;
+  line-height: 203px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const LettersListItem = styled.div`
+  width: 35.35vw;
+  border-radius: 17.84px;
+  background-color: #F9F9F9;
+  margin: 0 auto;
+  margin-top: 32px;
+  display: flex;
+`;
+
+export const LetterListItemCircle = styled.div`
+  width: 80.27px;
+  height: 80.27px;
+  margin: 12px 24px;
+  border-radius: 50%;
+  background: linear-gradient(93.5deg, #A2DA00 26.2%, #93C600 99.21%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 29px;
+  font-weight: 600;
+  line-height: 38px;
+  letter-spacing: 0em;
+  text-align: center;
+`;
+
+export const LettersListItemTextGroup = styled.div`
+  margin: auto 0;
+`;
+
+export const LettersListItemTextOne = styled.div`
+  font-size: 29px;
+  font-weight: 600;
+  line-height: 38px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const LettersListItemTextTwo = styled.div`
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 38px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const LettersListItemArrowIcon = styled(Image)`
+  margin: auto 0;
+  margin-left: auto;
+  margin-right: 25px;
+  width: 1.67vw;
+  height: 1.98vh;
+`;
+
+export const BackToLettersButton = styled.div`
+  margin-top: 20px;
+  width: 276px;
+  height: 83px;
+  border: 1px solid;
+  border-color: #878787;
+  border-radius: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+  font-weight: 500;
+  line-height: 37px;
+  letter-spacing: 0em;
+  text-align: center;
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
-import { SectionHeader, ChooseTheFirstLetter, LetterSelection, LetterCircle, LetterRow, LineBetweenLetters, AdvertisementRectangle, AdvertisementImageTag, AdvertisementText, ListOfSymbolsForLetter } from "./page.styled";
+import { SectionHeader, ChooseTheFirstLetter, LetterSelection, LetterCircle, LetterRow, LineBetweenLetters, AdvertisementRectangle, AdvertisementImageTag, AdvertisementText, ListOfSymbolsForLetterSection, LettersList, SelectedLetterSection, LettersListItem, LetterListItemCircle, LettersListItemTextGroup, LettersListItemTextOne, LettersListItemTextTwo, LettersListItemArrowIcon, BackToLettersButton } from "./page.styled";
 import Image from 'next/image';
 import GreenVector from "../../public/greenVector.png";
 import AdvertisementImage from "../../public/advertisement.png";
+import ArrowIcon from "../../public/arrow-icon.png"
 
 export default function Home() {
   const firstRowAlphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   const secondRowAlphabets = ['I', 'J', 'K', 'L', 'M', 'N', 'O'];
   const thirdRowAlphabets = ['P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'];
   const fourthRowAlphabets = ['X', 'Y', 'Z'];
+
+  const renderLettersListItems = () => {
+    const itemsToRender = 22;
+    const items = [];
+
+    for (let i = 0; i < itemsToRender; i++) {
+      items.push(
+        <LettersListItem key={i}>
+          <LetterListItemCircle>{i + 1}</LetterListItemCircle>
+          <LettersListItemTextGroup>
+            <LettersListItemTextOne>Lorem ipsum</LettersListItemTextOne>
+            <LettersListItemTextTwo>21 Dreams found</LettersListItemTextTwo>
+          </LettersListItemTextGroup>
+          <LettersListItemArrowIcon src={ArrowIcon} alt="< >" />
+        </LettersListItem>
+      );
+    }
+
+    return items;
+  };
 
   return (
   //  <main>
@@ -16,7 +37,7 @@ export default function Home() {
   <div className="dictionaryOfDreams">
     <ChooseTheFirstLetter>
       <SectionHeader>
-        Choose the first letter.
+        Choose the first letter
       </SectionHeader>
       <Image src={GreenVector} alt="Green Vector" />
       <LetterSelection>
@@ -59,9 +80,24 @@ export default function Home() {
         <AdvertisementImageTag src={AdvertisementImage} layout="fill" alt="Advertisement" />
       </AdvertisementRectangle>
     </ChooseTheFirstLetter>
-    <ListOfSymbolsForLetter>
-
-    </ListOfSymbolsForLetter>
+    <ListOfSymbolsForLetterSection>
+      <SectionHeader>
+        List of Symbols for Letter
+      </SectionHeader>
+        {/* <SelectedLetterSection> Need to fix
+          R 
+        </SelectedLetterSection> */}
+      <LettersList>
+        {renderLettersListItems()}
+      </LettersList>
+      <BackToLettersButton>Back To Letters</BackToLettersButton>
+      <AdvertisementRectangle>
+        <AdvertisementText>
+          Advertisement
+        </AdvertisementText>
+        <AdvertisementImageTag src={AdvertisementImage} layout="fill" alt="Advertisement" />
+      </AdvertisementRectangle>
+    </ListOfSymbolsForLetterSection>
   </div>
   )
 }
